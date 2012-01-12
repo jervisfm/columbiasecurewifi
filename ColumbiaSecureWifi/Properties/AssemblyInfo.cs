54b311aa4af9386260d26e5aff8331c168a01878
Jervis Muindi <jervis625@hotmail.com>
2012-01-12 22:53:32 +0300

    added icon

--- ColumbiaSecureWifi/Properties/AssemblyInfo.cs
+++ ColumbiaSecureWifi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ColumbiaSecureWifi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tunnels network traffic through Columbia servers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Muindi INc")]
 [assembly: AssemblyProduct("ColumbiaSecureWifi")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
