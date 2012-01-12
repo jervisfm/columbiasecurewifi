dc99a427500018bb7105e417a0b1a6aed88743c9
Jervis Muindi <jervis625@hotmail.com>
2012-01-12 23:27:21 +0300

    created a basic ui with login screen

--- ColumbiaSecureWifi/Form1.Designer.cs
+++ ColumbiaSecureWifi/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_Username = new System.Windows.Forms.Label();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Credentials";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.textBox_Password);
+            this.panel1.Controls.Add(this.label_Password);
+            this.panel1.Controls.Add(this.textBox_Username);
+            this.panel1.Controls.Add(this.label_Username);
+            this.panel1.Location = new System.Drawing.Point(17, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(285, 143);
+            this.panel1.TabIndex = 0;
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Location = new System.Drawing.Point(7, 18);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(82, 15);
+            this.label_Username.TabIndex = 0;
+            this.label_Username.Text = "Columbia UNI";
+            this.label_Username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Location = new System.Drawing.Point(96, 16);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(148, 23);
+            this.textBox_Username.TabIndex = 1;
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(7, 56);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(57, 15);
+            this.label_Password.TabIndex = 0;
+            this.label_Password.Text = "Password";
+            this.label_Password.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(81, 99);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(134, 29);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(96, 54);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.PasswordChar = '*';
+            this.textBox_Password.Size = new System.Drawing.Size(148, 23);
+            this.textBox_Password.TabIndex = 1;
+            this.textBox_Password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(351, 304);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Columbia Secure Wifi";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_Username;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox textBox_Password;
     }
 }
 
--- ColumbiaSecureWifi/Form1.cs
+++ ColumbiaSecureWifi/Form1.cs
@@ -15,5 +15,20 @@ namespace ColumbiaSecureWifi
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
